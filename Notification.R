c00484dbd4142bce3c0865bdd16412b3f328d369
Supriyabiradarpatil <supriybiradarpatil@gmail.com>
2024-11-10 23:40:42 +0530

    Add files via upload

---
+++ Notification.R
@@ -0,0 +1,19 @@
+library(shiny)
+library(shinyFeedback)
+
+ui <- fluidPage(
+  actionButton("goodnight", "Good night")
+)
+server <- function(input, output, session) {
+  observeEvent(input$goodnight, {
+    showNotification("So long")
+    Sys.sleep(1)
+    showNotification("Farewell")
+    Sys.sleep(1)
+    showNotification("Auf Wiedersehen")
+    Sys.sleep(1)
+    showNotification("Adieu")
+  })
+}
+shinyApp(ui, server)
+
